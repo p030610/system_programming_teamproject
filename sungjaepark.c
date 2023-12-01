f37625a28e929b2eeea0260576861295959cbdf1
박성재 <p030610@naver.com>
2023-12-01 22:04:36 +0900

    sungjaepark.c init

---
+++ sungjaepark.c
@@ -0,0 +1,32 @@
+// 시프실 팀플용 프로젝트 코드 - 박성재
+// 담당 모듈 : 
+
+// 펠티어	
+// 펌프	
+// 차양막	
+// lcd	
+// led
+
+
+#include <stdio.h>
+
+int peltier()
+{
+    return 0;
+}
+int pump()
+{
+    return 0;
+}
+int lcd()
+{
+    return 0;
+}
+int led()
+{
+    return 0;
+}
+int main()
+{
+    return 0;
+}
