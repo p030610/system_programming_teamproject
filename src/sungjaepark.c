0f12ba1ce619941459cd26764cb46e306ecfea0f
박성재 <p030610@naver.com>
2023-12-07 10:09:31 +0900

    sungjaepark.c commit

--- src/sungjaepark.c
+++ src/sungjaepark.c
@@ -3,18 +3,203 @@
 
 // 펠티어	
 // 펌프	
-// 차양막	
+// 차양막(dc모터)	
 // lcd	
-// led
 
+#define MT_IN1 3
+#define MT_IN2 4
+#define MT_IN3 5
+#define MT_IN4 6
+
+#include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+#define IN 0
+#define OUT 1
+#define PWM 0
+
+#define LOW 0
+#define HIGH 1
+#define VALUE_MAX 256
+#define DIRECTION_MAX 256
+
+static int PWMExport(int pwmnum) {
+#define BUFFER_MAX 3
+  char buffer[BUFFER_MAX];
+  int fd, byte;
+
+  // TODO: Enter the export path.
+  fd = open("/sys/class/pwm/pwmchip0/export", O_WRONLY);
+  if (-1 == fd) {
+    fprintf(stderr, "Failed to open export for export!\n");
+    return (-1);
+  }
+
+  byte = snprintf(buffer, BUFFER_MAX, "%d", pwmnum);
+  write(fd, buffer, byte);
+  close(fd);
+
+  sleep(1);
+
+  return (0);
+}
+
+static int PWMEnable(int pwmnum) {
+  static const char s_enable_str[] = "1";
+
+  char path[DIRECTION_MAX];
+  int fd;
+
+  // TODO: Enter the enable path.
+  snprintf(path, DIRECTION_MAX, "/sys/class/pwm/pwmchip0/pwm0/enable", pwmnum);
+  fd = open(path, O_WRONLY);
+  if (-1 == fd) {
+    fprintf(stderr, "Failed to open in enable!\n");
+    return -1;
+  }
+
+  write(fd, s_enable_str, strlen(s_enable_str));
+  close(fd);
+
+  return (0);
+}
+
+static int PWMWritePeriod(int pwmnum, int value) {
+  char s_value_str[VALUE_MAX];
+  char path[VALUE_MAX];
+  int fd, byte;
+
+  // TODO: Enter the period path.
+  snprintf(path, VALUE_MAX, "/sys/class/pwm/pwmchip0/pwm0/period", pwmnum);
+  fd = open(path, O_WRONLY);
+  if (-1 == fd) {
+    fprintf(stderr, "Failed to open in period!\n");
+    return (-1);
+  }
+  byte = snprintf(s_value_str, VALUE_MAX, "%d", value);
+
+  if (-1 == write(fd, s_value_str, byte)) {
+    fprintf(stderr, "Failed to write value in period!\n");
+    close(fd);
+    return -1;
+  }
+  close(fd);
+
+  return (0);
+}
+
+static int PWMWriteDutyCycle(int pwmnum, int value) {
+  char s_value_str[VALUE_MAX];
+  char path[VALUE_MAX];
+  int fd, byte;
+
+  // TODO: Enter the duty_cycle path.
+  snprintf(path, VALUE_MAX, "/sys/class/pwm/pwmchip0/pwm0/duty_cycle", pwmnum);
+  fd = open(path, O_WRONLY);
+  if (-1 == fd) {
+    fprintf(stderr, "Failed to open in duty cycle!\n");
+    return (-1);
+  }
+  byte = snprintf(s_value_str, VALUE_MAX, "%d", value);
+
+  if (-1 == write(fd, s_value_str, byte)) {
+    fprintf(stderr, "Failed to write value in duty cycle!\n");
+    close(fd);
+    return -1;
+  }
+  close(fd);
+
+  return (0);
+}
+
+
+static int GPIOExport(int pin) {
+    #define BUFFER_MAX 3
+    char buffer[BUFFER_MAX];
+    ssize_t bytes_written;
+    int fd;
+
+    fd = open("/sys/class/gpio/export", O_WRONLY);
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open export for writing!\n");
+        return (-1);
+    }
+
+    bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pin);
+    write(fd, buffer, bytes_written);
+    close(fd);
+    return (0);
+}
+
+static int GPIODirection(int pin, int dir) {
+    static const char s_directions_str[] = "in\0out";
+
+    char path[DIRECTION_MAX];
+    int fd;
+
+    snprintf(path, DIRECTION_MAX, "/sys/class/gpio/gpio%d/direction", pin);
+    fd = open(path, O_WRONLY);
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open gpio direction for writing!\n");
+        return (-1);
+    }
+
+    if (-1 == write(fd, &s_directions_str[IN == dir ? 0 : 3], IN == dir ? 2 : 3)) {
+        fprintf(stderr, "Failed to set direction!\n");
+        return (-1);
+    }
+}
+
+static int GPIOWrite(int pin, int value) {
+    static const char s_values_str[] = "01";
+
+    char path[VALUE_MAX];
+    int fd;
+
+    snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
+    fd = open(path, O_WRONLY);
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open gpio value for writing!\n");
+        return (-1);
+    }
+
+    if (1 != write(fd, &s_values_str[LOW == value ? 0 : 1], 1)) {
+        fprintf(stderr, "Failed to write value!\n");
+        return (-1);
+    }
+
+    close(fd);
+    return (0);
+}
+
+static int GPIOUnexport(int pin) {
+    char buffer[BUFFER_MAX];
+    ssize_t bytes_written;
+    int fd;
+
+    fd = open("/sys/class/gpio/unexport", O_WRONLY);
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open unexport for writing!\n");
+        return (-1);
+    }
+
+    bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pin);
+    write(fd, buffer, bytes_written);
+    close(fd);
+    return (0);
+}
+
 
 int peltier()//추가하드웨어필요
 {
     return 0;
 }
 int pump()//추가하드웨어필요
-{
+{    
+
     return 0;
 }
 int lcd()
@@ -27,5 +212,55 @@ int led()
 }
 int main()
 {
+
+    PWMExport(0);
+    PWMWritePeriod(0, 25);
+    PWMWriteDutyCycle(0, 0);
+    PWMEnable(0);
+
+    if (GPIOExport(17) == -1) {
+    return 1;
+    }
+
+    if (GPIODirection(17, OUT) == -1) {
+        return 2;
+    }
+
+    if (GPIOWrite(17, 0) == -1) {
+        return 3;
+    }
+
+    while (1) {
+        PWMWriteDutyCycle(0, 25);
+        usleep(1000);
+    }
+
+
+    // if (GPIOExport(14) == -1) {
+    // return 1;
+    // }
+    
+
+    // if (GPIODirection(14, OUT) == -1) {
+    //     return 2;
+    // }
+    
+
+    // do {
+    //     if (GPIOWrite(15, 1) == -1) {
+    //     return 3;
+    //     }
+        
+    //     printf("abc");
+    //     usleep(10);
+    // } while (1);
+
+    // if (GPIOUnexport(14) == -1) {
+    //     return 4;
+    // }
+    // if (GPIOUnexport(17) == -1) {
+    //     return 4;
+    // }
+
     return 0;
 }
